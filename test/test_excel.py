9fffafe95954562a9c3575fc23e7de9bbcb216b6
YugoNogami <hogehoge@example.com>
2019-01-27 17:43:16 +0900

    update Multiple file processing

--- test/test_excel.py
+++ test/test_excel.py
@@ -1,15 +1,19 @@
 # coding:utf-8
 
-import sys,os
+import sys
+import os
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/../src')
 import excel
 
 
+filename = os.path.dirname(os.path.abspath(__file__)) + '/data/26-1-18.xlsx'
+
+
 class TestExcel(object):
     def test_add_number(self):
-        test_excel = excel.Excel('./data/26-1-18.xlsx')
-        assert test_excel.add_number(50, 10)== 60
+        test_excel = excel.Excel(filename)
+        assert test_excel.add_number(50, 10) == 60
 
     def test_get_value(self):
-        test_excel = excel.Excel('./data/26-1-18.xlsx')
-        assert test_excel.get_value(10,0) ==  u'島部'
+        test_excel = excel.Excel(filename)
+        assert test_excel.get_value(10, 0) == '島部'
